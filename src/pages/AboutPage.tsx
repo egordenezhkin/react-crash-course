9fc4078dfe4e3304e929728cbb47465d65b8d435
Egor <egordenezhkin@gmail.com>
2023-12-15 11:30:27 +0100

    Initial commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam eveniet ad
+      asperiores impedit quas pariatur non mollitia officia qui, possimus vitae
+      sit dicta necessitatibus facere commodi aspernatur totam laboriosam. Eos.
+    </div>
+  );
+}
